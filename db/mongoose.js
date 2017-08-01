876d36412bc308d01190f087c050bf7d1e2ad552
Makiko Shukunobe <m_shukunobe@hotmail.com>
2017-08-01 10:37:10 -0400

    MongoDB database name changed

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //const uri = 'mongodb://localhost:27017/gridfstest';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/gridfstest');
+mongoose.connect('mongodb://localhost:27017/tld');
 //mongoose.createConnection(uri);
+module.exports = {mongoose};
 module.exports = {mongoose};
