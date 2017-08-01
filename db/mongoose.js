fb9c677b34974aebacae338f95f7314134f31b42
Makiko Shukunobe <m_shukunobe@hotmail.com>
2017-08-01 10:51:15 -0400

    Cleaning code

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-//const uri = 'mongodb://localhost:27017/gridfstest';
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/tld');
 //mongoose.createConnection(uri);
