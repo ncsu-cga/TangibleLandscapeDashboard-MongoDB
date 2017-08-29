07635da08dc35c8cfbaea988f011bddb128ef202
Makiko Shukunobe <m_shukunobe@hotmail.com>
2017-08-29 13:17:44 -0400

    Changed development DB to tld.

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ let env = process.env.NODE_ENV || 'development';
 console.log('*****env', env);
 if (env === 'development') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/gridfstest'
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/tld'
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/gridfstest'
