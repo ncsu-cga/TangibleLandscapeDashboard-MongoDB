4a33b7674078f16c865b5dd6e93f2f196ddab68b
Makiko Shukunobe <m_shukunobe@hotmail.com>
2017-08-31 11:10:41 -0400

    Modified config to run test-client

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ console.log('*****env', env);
 if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/tld'
-} else if (env === 'test') {
+} else if (env === 'test' || env === 'test-client') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/gridfstest'
     console.log('MONGODB TEST');
